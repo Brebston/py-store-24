0e5cdaa70cd6e87db78d491ca95c0edbdcf8138a
Yurii Palahitskyi <brebston7@gmail.com>
2026-03-29 14:11:48 +0200

    feat: integrate DRF for products API and improve URL handling
    Added DRF ViewSet and serializers for Product model. Registered Product API URLs using a router. Updated `products` app URLs to include DRF routes. Enhanced products template and fixed URL usage with `{% url %}`. Updated `requirements.txt` with necessary DRF-related packages and dependencies. Minor styling adjustment for address labels.

---
+++ catalog/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from catalog.models import Category, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            "name",
+            "slug",
+            "description",
+            "is_active",
+        ]
+
+
+class ProductSerializer(serializers.ModelSerializer,):
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "slug",
+            "short_description",
+            "description",
+            "price",
+            "stock",
+            "image",
+            "category",
+            "highlights",
+            "included",
+            "specs",
+            "is_active",
+            "created_at",
+        ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+
 from catalog.models import Product
+from catalog.serializers import ProductSerializer
 
 
 class CatalogView(ListView):
     model = Product
     template_name = "catalog/products.html"
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """DRF ViewSet for Product model."""
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "slug"
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            self.permission_classes = []
+        else:
+            self.permission_classes = [IsAdminUser]
+        return [permission() for permission in self.permission_classes]
 
 
 class ProductDetailView(DetailView):
